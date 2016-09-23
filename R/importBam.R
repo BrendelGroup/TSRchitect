b9867f8f78130b7163d7f4e27ac883e90362ccd7
rtraborn <rtraborn@indiana.edu>
2016-09-23 17:46:03 -0400

    Added base set of commands for import of TSS data plus basic package components.

---
+++ R/importBam.R
@@ -0,0 +1,58 @@
+#' Computes TSS positions from all from a tssExp object
+#' @param expName an S4 object of class tssExp that contains information about the experiment.
+
+#' @importFrom BiocParallel bplapply MulticoreParam
+#' @importFrom GenomicAlignments readGAlignments
+#' @importFrom Rsamtools scanBamFlag ScanBamParam BamViews
+#' @export 
+
+setGeneric(
+    name="importBam",
+    def=function(expName) {
+        standardGeneric("importBam")
+    }
+    )
+
+setMethod("importBam",
+          signature(expName="tssExp"),
+
+          function(expName) {
+
+              expName.chr <- deparse(substitute(expName))
+
+              exp.type <- expName@dataType
+
+             if(exp.type=="pairedEnd") {
+                  scanBamFlag(isPaired=TRUE, isProperPair=TRUE, isUnmappedQuery=FALSE) -> bamFlags
+                  cat("\nPaired-end TSS data specified.")
+                  c("rname","strand","pos","qwidth", "mapq", "isize") -> myFields
+              }
+                                     
+              else {
+                  scanBamFlag(isPaired=FALSE, isProperPair=FALSE, isUnmappedQuery=FALSE) -> bamFlags
+                  cat("\nSingle-end TSS data specified.")
+                  c("rname","strand","pos", "qwidth", "mapq") -> myFields
+              }
+
+              my.param <- ScanBamParam(flag=bamFlags, what=myFields)
+
+              bam.paths <- expName@fileNames
+              
+              bv_obj <- BamViews(bam.paths)
+              bv_files <- dimnames(bv_obj)[[2]]
+              n.bams <- length(bv_files)
+              
+              cat("Began import of", n.bams, "bam files.\n")
+              
+              bams.GA <- bplapply(bam.paths, readGAlignments, BPPARAM = MulticoreParam(),param=my.param)
+
+              expName@bamData <- bams.GA
+              
+              cat("Import complete!\n")
+              
+              cat("Alignment data from", n.bams, "bams has been attached to your tssExp object.\n")
+
+              assign(expName.chr, expName, parent.frame()) 
+              
+          }
+          )
---
+++ R/initializeExp.R
@@ -0,0 +1,62 @@
+#' Initializes the TSS profiling experiment
+#' @param expTitle a title for the experiment (chracter)
+#' @param objName a name the object to be created in your working environment (character)
+#' @param expDir a path to the directory with the alignment files in bam format (character)
+#' @param isPairedEnd a logical indicating whether the TSS profiling experiment was single or paired-end
+#'
+#' @return Creates a tssExp object in the user's current workspace.
+#' @export 
+
+setGeneric(
+    name="initializeExp",
+    def=function(expTitle, objName, expDir, isPairedEnd) {
+        standardGeneric("initializeExp")
+    }
+    )
+
+setMethod("initializeExp",
+          signature(expTitle="character", objName="character", expDir="character", isPairedEnd="logical"),
+          function(expTitle, objName, expDir, isPairedEnd) {
+
+              tssObj <-new("tssExp")
+              
+              if (is.na(expTitle)) {
+                  stop("Argument 'expTitle' is empty. Please supply a title for your experiment.")
+              }
+
+              if (is.na(objName)) {
+                  stop("Argument 'objName' is empty. Please supply a name for the tssExp object.")
+                   }
+
+              if (is.na(expDir)) {
+                 stop("Argument 'expDir' is empty. Please supply the full paths location of the alignment files (bams) for this experiment.")
+                  }
+                      
+              if (isPairedEnd==TRUE) {
+                  tssObj@dataType <- c("pairedEnd")
+              }
+
+              else {
+                  tssObj@dataType <- c("singleEnd")
+              }
+
+              tss_files <- list.files(expDir, pattern="\\.bam$", all.files=FALSE,full.names=TRUE)
+
+              print(expTitle)
+              
+              tssObj@title <- expTitle
+
+              tssObj@fileNames <- tss_files
+
+              cat("The tssExp object:", objName, "is now in your workspace. \n")
+
+              assign(objName, tssObj, parent.frame()) 
+              
+          }
+          )
+
+              
+              
+
+   
+    
