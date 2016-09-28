27082f9176f119eac38ec8ec30793e024e5db501
rtraborn <rtraborn@indiana.edu>
2016-09-28 19:19:53 -0400

    Updated location of BAM files in demo.

--- demo/demo-CAGE.R
+++ demo/demo-CAGE.R
@@ -1,8 +1,9 @@
-### note: before you begin this test, please copy the contents of /DATA/cage-demo/ into /extdata/CAGE
-library(TSRchitect)
+### note: before you begin this test, please copy the contents of /DATA/TSRchitect-demo/ into /extdata/CAGE
+### This demo should be run from the top-level directory of TSRchitect (TSRchitect/)
 ####################################################################################################
+library(TSRchitect)
 # initializing the tssExp object                                                                   
-initializeExp("Example human CAGE experiment", "CAGEhuman", c("/extdata/CAGE"), isPairedEnd=FALSE) #
+\initializeExp("Example human CAGE experiment", "CAGEhuman", c("/extdata/CAGE"), isPairedEnd=FALSE) #
 ####################################################################################################
 # loading the BAM files from the TSS profiling experiment (in this case, CAGE)                     
 importBam(CAGEhuman)                                                                               #
