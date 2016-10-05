ad2ddff228a6cf290c6072ce18d0af77d6ced18c
rtraborn <rtraborn@indiana.edu>
2016-10-05 16:04:46 -0400

    Updates to demo/. Added RAMPAGE demo and included installation from devtools.

--- demo/demo-RAMPAGE.R
+++ demo/demo-RAMPAGE.R
@@ -1,4 +1,4 @@
-### note: before you begin this test, please copy the contents of /DATA/TSRchitect-demo/ into extdata/CAGE
+### note: before you begin this demo, please copy the contents of /DATA/TSRchitect-demo/RAMPAGE into extdata/RAMPAGE.
 ### This demo should be run from the top-level directory of TSRchitect (TSRchitect/)
 ####################################################################################################
 # Installation of TSRchitect from source
@@ -7,14 +7,14 @@ devtools::install_github("brendelgroup/TSRchitect")
 library(TSRchitect)
 ####################################################################################################
 # initializing the tssExp object                                                                   
-initializeExp("Example human CAGE experiment", "CAGEhuman", c("extdata/CAGE"), isPairedEnd=FALSE) #
+initializeExp("Example RAMPAGE experiment (Rice)", "riceRAMPAGE", "bam_filtered", isPairedEnd=TRUE)#
 ####################################################################################################
-# loading the BAM files from the TSS profiling experiment (in this case, CAGE)                     
-importBam(CAGEhuman)                                                                               #
+# loading the BAM files from the TSS profiling experiment (in this case, RAMPAGE)                     
+importBam(riceRAMAPGE)                                                                             #
 ####################################################################################################
 # converting BAM data into TSS information and attaching it to your tssExp object                  
-bamToTSS(CAGEhuman)                                                                                #
+bamToTSS(riceRAMPAGE)                                                                              #
 ####################################################################################################
-# finding TSRs for a given dataset
-tsrFind(expName=CAGEhuman, tssNum=1, clustDist=20, nTSSs=5)
+# finding TSRs for a given dataset (in this case, the first one)
+tsrFind(expName=RiceRAMPAGE, tssNum=1, clustDist=20, nTSSs=3)				           #
 ####################################################################################################
