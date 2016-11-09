4401a229d8defcf052db43a90330fddc11e3c561
rtraborn <rtraborn@indiana.edu>
2016-11-09 15:21:55 -0500

    Corrected error in tssExp prototype from last commit.

--- R/Accessor-methods.R
+++ R/Accessor-methods.R
@@ -1,3 +1,5 @@
+#' exportClasses(TSRchitect)
+
 setGeneric(
 name="getTitle",
 def=function(expName){
@@ -72,7 +74,7 @@ setGeneric(
 name="getTSRs",
 def=function(expName){
 	standardGeneric("getTSRs")
-}
+    }
 )
 
 setMethod("getTSRs",
