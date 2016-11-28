de1505e97d532d70f0628bbe6eacc4e11a8f52dc
rtraborn <rtraborn@indiana.edu>
2016-11-28 15:41:31 -0500

    Added initilization of tsrData list to setSampleID. Other minor changes.

--- R/tsrToDF.R
+++ R/tsrToDF.R
@@ -6,7 +6,7 @@
 tsrToDF <- function(x) {
     len.list <- length(x)
     chr.vec <- names(x)
-    final.matrix <- matrix(NA, nrow=1, ncol=7)
+    final.matrix <- matrix(NA, nrow=1, ncol=6)
 
     for (j in 1:len.list) {
         vector(mode="list", length=2) -> my.list.p
@@ -15,43 +15,48 @@ tsrToDF <- function(x) {
         x[[j]] -> this.chr.list
         this.chr.list$plus -> this.p
         c("+") -> my.strand
-        matrix(NA, nrow=length(this.p), ncol=7) -> plus.matrix
+        #matrix(NA, nrow=length(this.p), ncol=7) -> plus.matrix
+        matrix(NA, nrow=length(this.p), ncol=6) -> plus.matrix
 
         for (i in 1:length(this.p)) {
             this.p[[i]] -> my.tsr
             countsToVector(my.tsr) -> my.vec
             shapeIndex(my.vec) -> my.SI
             round(my.SI, digits=2) -> my.SI
-            tsrCounts(my.tsr) -> my.counts
+            #tsrCounts(my.tsr) -> my.counts
             tsrWidth(my.tsr) -> my.width
             range(my.vec) -> my.range
             my.range[1] -> my.start
             my.range[2]+1 -> my.end
-            c(this.chr, my.start, my.end, my.strand, my.counts, my.width, my.SI) -> my.string
+            #c(this.chr, my.start, my.end, my.strand, my.counts, my.width, my.SI) -> my.string
+            c(this.chr, my.start, my.end, my.strand, my.width, my.SI) -> my.string
             my.string -> plus.matrix[i,]
         }
             this.chr.list$minus -> this.m
             c("-") -> my.strand
-            matrix(NA, nrow=length(this.m), ncol=7) -> minus.matrix
+            #matrix(NA, nrow=length(this.m), ncol=7) -> minus.matrix
+            matrix(NA, nrow=length(this.m), ncol=6) -> minus.matrix
 
         for (i in 1:length(this.m)){
             this.m[[i]] -> my.tsr
             countsToVector(my.tsr) -> my.vec
             shapeIndex(my.vec) -> my.SI
             round(my.SI, digits=2) -> my.SI
-            tsrCounts(my.tsr) -> my.counts
+            #tsrCounts(my.tsr) -> my.counts
             tsrWidth(my.tsr) -> my.width
             range(my.vec) -> my.range
             my.range[1]-1 -> my.start
             my.range[2] -> my.end
-            c(this.chr, my.start, my.end, my.strand, my.counts, my.width, my.SI) -> my.string
+            #c(this.chr, my.start, my.end, my.strand, my.counts, my.width, my.SI) -> my.string
+            c(this.chr, my.start, my.end, my.strand, my.width, my.SI) -> my.string
             my.string -> minus.matrix[i,]
         }
     rbind(plus.matrix, minus.matrix) -> chr.matrix
     rbind(final.matrix, chr.matrix) -> final.matrix
     }
     final.matrix <- final.matrix[-1,] #removes the empty first row used to initilize the matrix
-    colnames(final.matrix) <- c("chr", "start", "end", "strand", "nTSSs", "tsrWidth", "shapeIndex")
+    #colnames(final.matrix) <- c("chr", "start", "end", "strand", "nTSSs", "tsrWidth", "shapeIndex")
+    colnames(final.matrix) <- c("chr", "start", "end", "strand", "tsrWidth", "shapeIndex")
     final.df <- as.data.frame(final.matrix)
     return(final.df)
 }
