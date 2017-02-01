e6d164d638574980cd5a7b6820182b3553bcb814
rtraborn <rtraborn@indiana.edu>
2017-02-01 12:30:20 -0500

    Added documentation and exported two accessor functions for regular package use.

--- R/Get-methods.R
+++ R/Get-methods.R
@@ -1,3 +1,20 @@
+#' @title \strong{getTitle}
+#' @description an accessor function that retrieves the contents of slot "title"
+#' from a given \emph{tssObject}
+#'
+#' @param experimentName n S4 object of class \emph{tssObject}
+#' 
+#' @return the contents of slot "title" are returned
+#'
+#' @keywords methods
+#' 
+#' @examples
+#' load(system.file("extdata", "tssObjectExample.RData",
+#' package="TSRchitect"))
+#' example.title <- getTitle(experimentName=tssObjectExample)
+#' example.title
+#'
+#' @export
 
 setGeneric(
 name="getTitle",
@@ -12,6 +29,24 @@ function (experimentName){
 	experimentName@title
 }
 )
+
+#' @title \strong{getFileNames}
+#' @description an accessor function that retrieves the contents of slot "fileNames"
+#' from a given \emph{tssObject}
+#'
+#' @param experimentName n S4 object of class \emph{tssObject}
+#' 
+#' @return the contents of slot "fileNames" are returned
+#'
+#' @keywords methods
+#' 
+#' @examples
+#' load(system.file("extdata", "tssObjectExample.RData",
+#' package="TSRchitect"))
+#' example.file.names <- getFileNames(experimentName=tssObjectExample)
+#' example.file.names
+#'
+#' @export
 
 setGeneric(
 name="getFileNames",
